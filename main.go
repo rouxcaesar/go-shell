494c2937f5939dadcc4be67a9a46014b14a9debf
Julius Zerwick <jrzerwick@gmail.com>
2019-04-21 18:12:18 -0400

    Modified execInput to accept multiple arguments for command flags

--- main.go
+++ main.go
@@ -4,39 +4,41 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-  "os/exec"
-  "strings"
+	"os/exec"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
-    fmt.Print("> ")
+		fmt.Print("> ")
 		// Read the keyboard input.
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 
-    // Handle the execution of the input.
-    if err = execInput(input); err != nil {
-      fmt.Fprintln(os.Stderr, err)
-    }
+		// Handle the execution of the input.
+		if err = execInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
 	}
 }
 
 func execInput(input string) error {
-  // Remove the newline character
-  input = strings.TrimSuffix(input, "\n")
+	// Remove the newline character
+	input = strings.TrimSuffix(input, "\n")
 
-  // Prepare the command to execute
-  cmd := exec.Command(input)
+	// Split the input to separate the command and the arguments.
+	args := strings.Split(input, " ")
 
-  // Set the correct output device.
-  cmd.Stderr = os.Stderr
-  cmd.Stdout = os.Stdout
+	// Pass the program and arguments separately.
+	cmd := exec.Command(args[0], args[1:]...)
 
-  // Execute the command and return the error.
-  return cmd.Run()
+	// Set the correct output device.
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	// Execute the command and return the error.
+	return cmd.Run()
 }
-
